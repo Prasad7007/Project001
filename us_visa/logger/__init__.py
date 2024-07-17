95c2bb533b331ef0e35bee77dd13b351827a659b
Prasad7007 <prasadagalave0007@gmail.com>
2024-07-17 17:49:12 +0530

    updated

--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -1,20 +1,22 @@
-import logging
 import os
-
-from from_root import from_root
+import logging
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Set the root directory manually
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+log_directory = os.path.join(root_dir, 'logs')
 
-log_dir = 'logs'
+# Create the directory if it does not exist
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
 
-logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
+# Define the log file name with the current timestamp
+log_filename = os.path.join(log_directory, datetime.now().strftime("%m_%d_%Y_%H_%M_%S.log"))
 
-os.makedirs(log_dir, exist_ok=True)
-
-
+# Configure logging
 logging.basicConfig(
-    filename=logs_path,
-    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    filename=log_filename,
     level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
