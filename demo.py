e20e2ee53351ded0cc0921c10c1bcc84497dc465
Prasad7007 <prasadagalave0007@gmail.com>
2024-07-20 12:40:53 +0530

    data ingetion->data validation->data transformation->model trainer

--- demo.py
+++ demo.py
@@ -1,9 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-logging.info("Custom Log")
-
-try:
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e, sys)
+obj = TrainPipeline()
+obj.run_pipeline()
